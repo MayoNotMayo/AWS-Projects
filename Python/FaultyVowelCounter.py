a303928dc1f25a0d0a1c92031ed9920adc958c6b
MayoNotMayo <kaberenada@gmail.com>
2024-05-12 18:47:49 +0900

    added notes to FaultyVowelCounter

--- Python/FaultyVowelCounter.py
+++ Python/FaultyVowelCounter.py
@@ -3,13 +3,16 @@ A flawed VowelCounter that only excludes lower case letters
 but also counts special characters any upper case letters.
 """
 
+#take user string input
 txt = str(input())
 
+#z is used to remove characters from the input
 x = ""
 y = ""
 z = "bcdfghjklmnpqrstvwxyz"
 
+#assigns mytable to the translation of txt
 mytable = str.maketrans(x, y, z)
 
-
+#prints the length of txt after deleting the spaces from txt
 print(len(txt.translate(mytable).replace(" ", "")))
